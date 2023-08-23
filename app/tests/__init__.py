d27979af1f45a275533e82967fab1538530db616
Patryk Wróblewski <102994893+Patryk2001k@users.noreply.github.com>
2023-08-23 21:53:09 +0200

    Search_for_robots

--- app/tests/__init__.py
+++ app/tests/__init__.py
@@ -1,15 +1,1 @@
 import pytest
-
-
-def test_home_route_shows_start_page(client):
-    with client:
-        response = client.get("/")
-        assert response.status_code == 200
-        assert b"Zapraszamy" in response.data
-
-
-def test_lore_route_shows_lore_page(client):
-    with client:
-        response = client.get("/lore")
-        assert response.status_code == 200
-        assert b"Lore strony" in response.data
