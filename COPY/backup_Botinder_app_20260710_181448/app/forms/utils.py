3168e7d191348c4571de8fd8e26fa60e3de8b46a
Patryk Wróblewski <patryk.wroblewski@poczta.onet.pl>
2026-07-10 18:22:06 +0200

    refactor: black part 2

--- COPY/backup_Botinder_app_20260710_181448/app/forms/utils.py
+++
@@ -1,8 +0,0 @@
-from app.models.enums import *
-
-
-def enum_to_choices(enum, replace):
-    if replace == True:
-        return [(str(i).replace("_", " "), str(i.value)) for i in enum]
-    else:
-        return [(str(j), str(j.value)) for j in enum]
